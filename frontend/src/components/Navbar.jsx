44de9fe0c27f6ef2a31d8af19a706c2670936747
Weather forecast <khandujanaman14@gmail.com>
2025-07-09 12:00:00 +0530

    logo added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import {assets} from '../assets/assets'
+import {NavLink} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-      
+      <img src = {assets.logo} alt = ""/>
+      <ul>
+      <NavLink>
+          <li>Home</li>
+          <hr/>
+      </NavLink>
+      <NavLink>
+          <li>All Doctors</li>
+          <hr/>
+      </NavLink>
+      <NavLink>
+          <li>About</li>
+          <hr/>
+      </NavLink>
+      <NavLink>
+          <li>Contact</li>
+          <hr/>
+      </NavLink>
+      </ul>
+      <div>
+        <button>create account</button>
+      </div>
     </div>
   )
 }
