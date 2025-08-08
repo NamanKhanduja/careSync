f2f1bad0f5cc5fa63dd7a47a09b10cc0855a5bcb
Weather forecast <khandujanaman14@gmail.com>
2025-08-08 23:36:47 +0530

    backend url changed

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = "http://localhost:4000"
+    const backendUrl = "https://care-sync-nu.vercel.app"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
